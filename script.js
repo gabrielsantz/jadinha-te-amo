88e476e660f6d0398a94ac722e0a147bc4457be6
Gabriel Santiago <101732762+gabrielsantz@users.noreply.github.com>
2022-12-11 00:40:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function lov() {
+    var bloco = document.getElementById ('block')
+    bloco.style.backgroundColor = 'red'
+    bloco.innerHTML = "<br><br><br><br><br>Te amo, minha gatinha"
+}
+function lovout() {
+    var bloco = document.getElementById ('block')
+    bloco.style.backgroundColor = 'black'
+    bloco.innerHTML = "<br><br><br><br><br>Passe o mouse aqui"
+}
